8dd69bc031c40bbf749c1dff6a1964bb5fbbd06b
Ben <monoganog@gmail.com>
2020-11-04 00:55:22 +1100

    Dice is showing text feedback

--- WobblyTechUnityProject/Assets/_Project/_Scripts/Dice.cs
+++ WobblyTechUnityProject/Assets/_Project/_Scripts/Dice.cs
@@ -2,24 +2,41 @@
 using System;
 using System.Security.Cryptography;
 using UnityEngine;
-
-
+using UnityEngine.UI;
 
 public class Dice : MonoBehaviour
 {
-    public Transform[] Numbers;
-
+    // array of the transforms of the sides. The actual number value of the side is array index + 1
+    public Transform[] Sides;
+    // reference to the rigidbody
     private Rigidbody rb;
 
     bool rollComplete;
+
+    public GameObject TextFeedback;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+
+        float rand = UnityEngine.Random.Range(-10.0f, 10f);
+        Vector3 randomRotation = new Vector3(rand, rand, rand);
+        rb.AddTorque(randomRotation* 300, ForceMode.Impulse);
     }
 
-    // Update is called once per frame
+    void SpawnTextFeedback(int rollValue)
+    {
+        Vector3 spawnLoacation = new Vector3(transform.position.x, this.transform.position.y + 1, this.transform.position.z);
+        GameObject TextFeedbackInstance = Instantiate(TextFeedback, spawnLoacation, Quaternion.identity, this.transform);
+        // make text look at camera
+        TextFeedbackInstance.GetComponentInChildren<Text>().text = rollValue.ToString();
+        
+        TextFeedbackInstance.transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+
+    // Update is called once per framed
     void Update()
     {
         // check to see if rigid body stopped moving
@@ -33,18 +50,25 @@ public class Dice : MonoBehaviour
 
                 int numberFacingDown = 0;
 
-                for (int i = 0; i < Numbers.Length; i++)
+               // for loop to iterate all the side transforms
+                for (int i = 0; i < Sides.Length; i++)
                 {
-                    if (Numbers[i].position.y < lowestYThisRoll)
+                    // check to see if the current side in the array has the lowest y value/ closest to the ground
+                    if (Sides[i].position.y < lowestYThisRoll)
                     {
-                        lowestYThisRoll = Numbers[i].position.y;
+                        lowestYThisRoll = Sides[i].position.y;
+
+                        // the number facing down is 
                         numberFacingDown = i + 1;
                     }
                 }
 
                 // sum of opposite sides add to 7 so can figure top number from 7 - bnumber facing down
-                int numberFacingUp = 7 - numberFacingDown; 
+                int numberFacingUp = 7 - numberFacingDown;
+                SpawnTextFeedback(numberFacingUp);
                 Debug.Log(numberFacingUp);
+
+                
                 rollComplete = true;
             }
         }

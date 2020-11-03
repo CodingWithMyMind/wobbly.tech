06dd0cb748931f8651c17697c8c371be28b89f00
Ben <monoganog@gmail.com>
2020-11-04 02:10:48 +1100

    dice ray works

--- WobblyTechUnityProject/Assets/_Project/_Scripts/DiceRay.cs
+++ WobblyTechUnityProject/Assets/_Project/_Scripts/DiceRay.cs
@@ -4,7 +4,7 @@ using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Dice : MonoBehaviour
+public class DiceRay : MonoBehaviour
 {
     // array of the transforms of the sides. The actual number value of the side is array index + 1
     public Transform[] Sides;
@@ -14,15 +14,17 @@ public class Dice : MonoBehaviour
     bool rollComplete;
 
     public GameObject TextFeedback;
+    private RaycastHit objectHit;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
+
         float rand = UnityEngine.Random.Range(-10.0f, 10f);
         Vector3 randomRotation = new Vector3(rand, rand, rand);
-        rb.AddTorque(randomRotation* 300, ForceMode.Impulse);
+        rb.AddTorque(randomRotation* 3000, ForceMode.Impulse);
     }
 
     void SpawnTextFeedback(int rollValue)
@@ -44,30 +46,32 @@ public class Dice : MonoBehaviour
             // check to see if the roll calculation is completed
             if (!rollComplete)
             {
-                // really high starting number that should always get replaced
-                float lowestYThisRoll = 1000;
-
                 int numberFacingDown = 0;
+                float shortestRay = 1000f;
 
                // for loop to iterate all the side transforms
                 for (int i = 0; i < Sides.Length; i++)
                 {
                     // check to see if the current side in the array has the lowest y value/ closest to the ground
-                    if (Sides[i].position.y < lowestYThisRoll)
-                    {
-                        lowestYThisRoll = Sides[i].position.y;
+                    //if (Sides[i].position.y < lowestYThisRoll)
+                    RaycastHit hit;
+                    //Vector3 fwd = Sides[i].transform.localPositio;
+                    Vector3 fwd = Sides[i].forward;
 
-                        // the number facing down is 
-                        numberFacingDown = i + 1;
+                    if (Physics.Raycast(Sides[i].transform.position, fwd, out hit, 50))
+                    {
+                        Debug.DrawRay(Sides[i].transform.position, fwd, Color.blue, 100.0f,false);
+                        if (hit.distance < shortestRay)
+                        {
+                            shortestRay = hit.distance;
+                            numberFacingDown = i + 1;
+                        }
                     }
                 }
-
                 // sum of opposite sides add to 7 so can figure top number from 7 - bnumber facing down
                 int numberFacingUp = 7 - numberFacingDown;
                 SpawnTextFeedback(numberFacingUp);
                 Debug.Log(numberFacingUp);
-
-                
                 rollComplete = true;
             }
         }

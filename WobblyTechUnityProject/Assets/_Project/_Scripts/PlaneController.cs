038996fc7d5e61389ea74b332e770cd06c6fce3d
Ben <monoganog@gmail.com>
2020-12-01 09:19:12 +1100

    Planes

---
+++ WobblyTechUnityProject/Assets/_Project/_Scripts/PlaneController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private float forwardSpeed = 5;
+
+    [SerializeField]
+    private float pitchSpeed = 10;
+
+    [SerializeField]
+    private float turnSpeed = 10;
+
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(transform.forward * Time.deltaTime * forwardSpeed);
+
+        transform.Rotate(Vector3.back * Time.deltaTime * turnSpeed * horizontalInput);
+
+        transform.Rotate(Vector3.left * Time.deltaTime * pitchSpeed * forwardInput);
+
+    }
+}

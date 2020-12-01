4a3a66af9d89756b29f0dc0fb69927d8db043616
Ben <monoganog@gmail.com>
2020-12-01 19:14:45 +1100

    Animal Game

---
+++ Assets/_Scripts/AnimalController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject food;
+    [SerializeField]
+    private float moveSpeed = 100;
+
+    [SerializeField]
+    private int xBounds = 5;
+
+
+
+    private float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject foodInstance = Instantiate(food, transform.position, food.transform.rotation);
+        }
+        
+        if (transform.position.x > xBounds)
+        {
+            transform.position = new Vector3(xBounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xBounds)
+        {
+            transform.position = new Vector3(-xBounds, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/_Scripts/AnimalSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class AnimalSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject[] animals;
+
+    [SerializeField]
+    int xRange;
+
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnAnimal", 3, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    void SpawnAnimal()
+    {
+        index = Random.Range(0, animals.Length);
+
+
+        int x = Random.Range(-xRange, xRange + 1);
+
+        Instantiate(animals[index], new Vector3(x, 1, 20), animals[index].transform.rotation);
+    }
+}

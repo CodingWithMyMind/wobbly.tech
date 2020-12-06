bd62ee268e021cabd97eeaefd14a0a36fc7d6ce8
Ben <monoganog@gmail.com>
2020-12-06 13:10:12 +1100

    Pattern moving rows not columns

--- Assets/_Scripts/PatternController.cs
+++ Assets/_Scripts/PatternController.cs
@@ -13,6 +13,10 @@ public class PatternController : MonoBehaviour
     public int rows = 3;
     public int columns = 3;
 
+    Vector3 lastInRowLocation;
+    Vector3 firstInRowLocation;
+
+
     public List<GameObject> spawnedIcons = new List<GameObject>();
 
     //public GameObject[] spawnedIcons;
@@ -24,22 +28,33 @@ public class PatternController : MonoBehaviour
         {
             for (int x = 0; x < rows; x++)
             {
+                
 
-                int randomIconIndex = UnityEngine.Random.Range(0,icons.Length);
-                GameObject instance = Instantiate(icons[randomIconIndex],canvas.transform);
-                Debug.Log("spawning x:" + x + " y:" + y);
+                
+                
+
 
                 Vector3 pos = new Vector3(x * 100, y * 100, 0);
                 Debug.Log(pos);
 
-                instance.transform.SetPositionAndRotation(pos, Quaternion.identity);
+                SpawnIcon(pos);
 
-                spawnedIcons.Add(instance);
+                
                 //instance.transform.position = pos;
                 //spawnedIcons[index] = instance;
                 index++;
             }
         }
+    }
+
+    private void SpawnIcon(Vector3 spawnPosition)
+    {
+        int randomIconIndex = UnityEngine.Random.Range(0, icons.Length);
+        GameObject instance = Instantiate(icons[randomIconIndex], canvas.transform);
+        Debug.Log("spawning at:" + spawnPosition);
+        instance.transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
+
+        spawnedIcons.Add(instance);
     }
 
 
@@ -60,17 +75,63 @@ public class PatternController : MonoBehaviour
 
     private void CheckIfInRow()
     {
+
         int row = UnityEngine.Random.Range(0, columns);
+
+        bool shouldMoveLeft = (UnityEngine.Random.Range(0f, 1f) > 0.5f);
+
+        float highestX = 0;
+        float lowestX = 10000;
+
+
+
+
         for (int i = 0; i < spawnedIcons.Count; i++)
         {
-            
             if(spawnedIcons[i].transform.position.y == row*100)
             {
-                spawnedIcons[i].transform.DOPunchScale(Vector3.one , 0.5f, 10, 10);
+                MoveRow(i,shouldMoveLeft);
+
+                if(spawnedIcons[i].transform.position.x > highestX)
+                {
+                    highestX = spawnedIcons[i].transform.position.x;
+                    lastInRowLocation = spawnedIcons[i].transform.position;
+
+                }
+                if (spawnedIcons[i].transform.position.x < lowestX)
+                {
+                    lowestX = spawnedIcons[i].transform.position.x;
+                    firstInRowLocation = spawnedIcons[i].transform.position;
+                }
             }
+
         }
+
+        if (!shouldMoveLeft)
+        {
+            SpawnIcon(lastInRowLocation);
+        }
+        if (shouldMoveLeft)
+        {
+            SpawnIcon(firstInRowLocation);
+        }
+
     }
 
+    private void MoveRow(int index,bool moveLeft)
+    {
+        Vector3 endMoveLocation;
+        if (moveLeft)
+        {
+            endMoveLocation = new Vector3(spawnedIcons[index].transform.position.x + 100, spawnedIcons[index].transform.position.y, spawnedIcons[index].transform.position.z);
+        }
+        else
+        {
+            endMoveLocation = new Vector3(spawnedIcons[index].transform.position.x - 100, spawnedIcons[index].transform.position.y, spawnedIcons[index].transform.position.z);
+        }
+        spawnedIcons[index].transform.DOMove(endMoveLocation, 1);
+
+    }
 
     private void CheckIfInColumn()
     {

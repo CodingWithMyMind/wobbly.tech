5917be6ab574b3f8eca123916b6288c636200a43
Ben <monoganog@gmail.com>
2020-12-14 21:03:27 +1100

    2 scenes getting ready to host properly

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public enum State { Menu, Main };
+
+    public GameObject MenuUI, MainUI;
+
+    //public GameObject MapNavUI, ARModeNavUI, LocationsNavUI, GalleryNavUI, InfoNavUI;
+
+
+
+    public State state;
+
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+        set
+        {
+            if (_instance == null)
+                _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+
+        //#if UNITY_STANDALONE_WIN
+        //        windowsBuild = true;
+        //#endif
+
+        //#if UNITY_IOS
+        //      IOSBuild = true;
+        //#endif
+
+        Instance = this;
+    }
+
+    void Start()
+    {
+        state = State.Main;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Menu:
+                {
+                    break;
+                }
+
+            case State.Main:
+                {
+                    break;
+                }
+        }
+    }
+
+    /* 
+     * Public Functions to be called from UI elements or from other scripts etc
+     * these will then change the game state to be ran every frame. Can also be 
+     * used like constructers to set up play states 
+    */
+
+
+
+
+    public void EnterMenu()
+    {
+
+
+        //TurnOnUI(MenuUI,MenuNavUI);
+
+        state = State.Menu;
+    }
+
+
+
+
+    // do once here
+
+    public void EnterMain()
+    {
+        //SceneManager.LoadSceneAsync("main", LoadSceneMode.Single);
+
+        StartCoroutine(AsynchronousLoad("Main"));
+
+        state = State.Main;
+    }
+
+IEnumerator AsynchronousLoad(string scene)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            // [0, 0.9] > [0, 1]
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+            // Loading completed
+            if (ao.progress == 0.9f)
+            {
+                Debug.Log("Press a key to start");
+                if (Input.anyKey)
+                    ao.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+
+}
